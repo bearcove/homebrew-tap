18e91c0f5055206d1a8da64649836369964e5678
beardist <amos@bearcove.eu>
2025-08-04 17:54:21 +0000

    Bump formulas: home to 3.7.0

--- Formula/home.rb
+++ Formula/home.rb
@@ -4,7 +4,7 @@
 class Home < Formula
   desc "A cozy content authoring experience"
   homepage "https://github.com/bearcove/home"
-  version "3.6.0"
+  version "3.7.0"
   license "Proprietary"
 
   depends_on "dav1d"
@@ -16,16 +16,16 @@ class Home < Formula
   depends_on "bearcove/tap/home-drawio"
 
   if OS.mac?
-    url "https://github.com/bearcove/home/releases/download/v3.6.0/aarch64-apple-darwin.tar.xz"
-    sha256 "2611844437a181b89c8156154fbd98312b6aa8a3e1897f7082b327e6a93328eb"
+    url "https://github.com/bearcove/home/releases/download/v3.7.0/aarch64-apple-darwin.tar.xz"
+    sha256 "d959c68030a2bf671c9d491c37965bdbc65780fb92374c9b768918830f6d462a"
   elsif OS.linux?
     on_intel do
-      url "https://github.com/bearcove/home/releases/download/v3.6.0/x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "5b32724cf8ad3167e824fa3a2411846b493bcb702080cec5e1bc207ad534bae8"
+      url "https://github.com/bearcove/home/releases/download/v3.7.0/x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "335bb4beac3a3b2c8210c61b04f896b069cbde647106d63bb54e9ec4748e2be5"
     end
     on_arm do
-      url "https://github.com/bearcove/home/releases/download/v3.6.0/aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "a0940c03613927b9d0dfeac372a9f2030421ed9a1fd7460d24adb7b3be734cb7"
+      url "https://github.com/bearcove/home/releases/download/v3.7.0/aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "9bdec915c00c99e1c6c55e5634d4883ea2fd6d75ea089290822e22b9eaee7f91"
     end
   end
 
