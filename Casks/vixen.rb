232afb719a1be107ddad7494de30b794c5820bf6
Amos Wenger <amos@bearcove.eu>
2026-02-12 22:17:51 +0100

    vixen: update to 0.6.0

--- Casks/vixen.rb
+++ Casks/vixen.rb
@@ -1,8 +1,8 @@
 cask "vixen" do
-  version "0.4.1"
-  sha256 "c33a40950628e19e9504106c517ebc50a805421c9f1294ad9fad07c6be71b6ad"
+  version "0.6.0"
+  sha256 "3a144a7a914cb7889fb9249e66c4a645cbd1dab2daa8e05bb937f866c36343a7"
 
-  url "https://github.com/bearcove/vixen-mac-app/releases/download/v0.4.1/Vixen-0.4.1.dmg"
+  url "https://github.com/bearcove/vixen-mac-app/releases/download/v0.6.0/Vixen-0.6.0.dmg"
   name "Vixen"
   desc "A Rust build engine with correct caching"
   homepage "https://vixen.rs"
